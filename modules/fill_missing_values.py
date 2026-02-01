d3ee34e21866a5e68a9bb0bf9dd758f9c88d9813
colinrodney <bigboi0000@gmail.com>
2026-02-01 11:23:49 -0600

    Add fill_missing_values.py module for import.

---
+++ modules/fill_missing_values.py
@@ -0,0 +1,19 @@
+# FILL BLANKS/ NULLS (This function may be used multiple times on a dataset - may need to be decoupled from original)
+def fill_missing_values(df_2):
+    try:
+        import numpy as np       
+    except:
+        print("EXCEPTION - numpy import not successfull.")
+    else:
+        df_2 = df_2.replace("nan", np.nan)
+        df_2.fillna("unknown")
+        # df_2["phone_number"] = df_2["phone_number"].replace("nan", np.nan)
+        # df_2["mobile_phone"] = df_2["mobile_phone"].fillna("NONE")
+        # df_2["office_phone"] = df_2["office_phone"].fillna("NONE")
+        # df_2["sales_ytd"] = df_2["sales_ytd"].fillna(0)
+
+
+        # # All other empty fields filled w/ np.nan
+        # # df_2 = df_2.fillna("NONE_NEW")
+        # df_2 = df_2.fillna(np.nan)
+        return df_2

d94a9bd0f2bff9415adde11bdf50484fbf157f92
colinrodney <bigboi0000@gmail.com>
2026-02-01 15:24:30 -0600

    Add/ removed necessary function calls/ variables in main.py

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ import modules.fill_missing_values
 # Invoke functions
 # modules.module1.func1()
 df_2 = modules.ingest_data.ingest_data()
-df_2 = modules.schema_validate()
+modules.schema_validate.validate_schema(df_2)
 df_2 = modules.duplicates.show_duplicates(df_2)
 # df_2 = modules.duplicates.drop_duplicates()
 df_2 = modules.email_validate.email_formatted_properly(df_2)
 df_2 = modules.fill_missing_values.fill_missing_values(df_2)
+
+print(df_2)
 

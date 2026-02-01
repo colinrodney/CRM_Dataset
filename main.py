bd9fea10671f0bd122ecd19476aa09e922e0b547
colinrodney <bigboi0000@gmail.com>
2026-02-01 15:08:03 -0600

    Added import module functionality + function calls for modules.schema_validate, modules.duplicates, modules.email_validate

--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 # Import test_directory package
 import modules
 
-# Import modules
-# import modules.module1
-import modules.fill_missing_values
+
 import modules.ingest_data
+import modules.schema_validate
+import modules.duplicates
+import modules.email_validate
+import modules.fill_missing_values
 
 # Invoke functions
 # modules.module1.func1()
 df_2 = modules.ingest_data.ingest_data()
-modules.fill_missing_values.fill_missing_values(df_2)
+df_2 = modules.schema_validate()
+df_2 = modules.duplicates.show_duplicates(df_2)
+# df_2 = modules.duplicates.drop_duplicates()
+df_2 = modules.email_validate.email_formatted_properly(df_2)
+df_2 = modules.fill_missing_values.fill_missing_values(df_2)
 

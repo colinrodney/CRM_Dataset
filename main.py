735849b7ba312f9e75e88c2dfb83b964cf6c4b05
colinrodney <bigboi0000@gmail.com>
2026-02-01 10:54:40 -0600

    Enabled module import + function call on ingest_data module + disabled imports + function calls on module1.func1() and module2.func2() in file main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 import modules
 
 # Import modules
-import modules.module1
-import modules.module2
-# import modules.ingest_data
+# import modules.module1
+# import modules.module2
+import modules.ingest_data
 
 # Invoke functions
-modules.module1.func1()
-modules.module2.func2()
-# modules.ingest_data.ingest_data()
+# modules.module1.func1()
+# modules.module2.func2()
+modules.ingest_data.ingest_data()
 

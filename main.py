9cdd2486a41fac9373e7b9e4d4315ef12a63e8ab
colinrodney <bigboi0000@gmail.com>
2026-02-01 11:30:37 -0600

    Add + enabled fill_missing_values module import + function call on main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import modules
 
 # Import modules
 # import modules.module1
-# import modules.module2
+import modules.fill_missing_values
 import modules.ingest_data
 
 # Invoke functions
 # modules.module1.func1()
-# modules.module2.func2()
 modules.ingest_data.ingest_data()
+modules.fill_missing_values.fill_missing_values()
 

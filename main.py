885fe5eaae9b20df539b3c3e90a8948838bdd026
colinrodney <bigboi0000@gmail.com>
2026-02-01 09:47:29 -0600

    First commit of data cleansing pipeline automations to pre-existing github repo

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Import test_directory package
+import test_directory
+
+# Import modules
+import test_directory.module1
+import test_directory.module2
+import test_directory.ingest_data
+
+# Invoke functions
+test_directory.module1.func1()
+test_directory.module2.func2()
+test_directory.ingest_data.ingest_data()
+

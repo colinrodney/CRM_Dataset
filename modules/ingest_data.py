30271b68b551fc8631862c76e5e1113198b790af
colinrodney <bigboi0000@gmail.com>
2026-02-03 06:27:30 -0600

    Add code to automatically look in data input folder and process all .csv files + write to ouput path.

--- modules/ingest_data.py
+++ modules/ingest_data.py
@@ -26,15 +26,29 @@ def ingest_data():
              print("EXCEPTION -os module import not successfull.")
         else:
             # DEFINE INPUT / OUPUT DIRECTORIES
-            input_folder = "raw_data"
+            input_folder = "data/raw_data"
             output_folder = "cleaned_data"
 
             # Create output folder if it doesn't exist
             os.makedirs(output_folder, exist_ok=True)
 
+            # PROCESS EACH CSV IN FOLDER
+
+        for filename in os.listdir(input_folder):
+            print("FILENAME:'\n'",filename)
+            if filename.endswith(".csv"):
+                file_path = os.path.join(input_folder, filename)
+                print("FILEPATH: " '\n', file_path)
+
             # df = pd.read_csv("test_directory/dirty_cafe_sales-csv.csv")
-            df = pd.read_csv(r"data/raw_data/messy_crm_dataset_before_cleansing.csv")
+            # df = pd.read_csv(r"data/raw_data/messy_crm_dataset_before_cleansing.csv")
+            df = pd.read_csv(file_path)
             df_2 = df.copy()
             df_2.info()
             print("ingest data function ran successfully! RETURNING file to function!")
+
+            output_path = os.path.join(output_folder, f"cleaned_{filename}")
+            print("OUTPUT_PATH: " '\n', output_path)
+            df_2.to_csv(output_path, index=False)
+            
             return df_2

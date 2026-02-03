8edd145b3649ce84a438138d17710d57abff9106
colinrodney <bigboi0000@gmail.com>
2026-02-03 06:14:43 -0600

    Moved create input/output directories using in-built Python os module from try block to else block (addressing pandas import failure issues)

--- modules/ingest_data.py
+++ modules/ingest_data.py
@@ -18,15 +18,16 @@ def ingest_data():
             import pandas as pd
             print("Pandas imported successfully!")
 
+        except:
+             print("EXCEPTION - Pandas import not successfull.")
+        else:
             # DEFINE INPUT / OUPUT DIRECTORIES
             input_folder = "raw_data"
             output_folder = "cleaned_data"
 
             # Create output folder if it doesn't exist
             os.makedirs(output_folder, exist_ok=True)
-        except:
-             print("EXCEPTION - Pandas import not successfull.")
-        else:
+            
             # df = pd.read_csv("test_directory/dirty_cafe_sales-csv.csv")
             df = pd.read_csv(r"data/raw_data/messy_crm_dataset_before_cleansing.csv")
             df_2 = df.copy()

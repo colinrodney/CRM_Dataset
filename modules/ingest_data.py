1a090f6d44d88ecfe511b5856995de05c18f6e89
colinrodney <bigboi0000@gmail.com>
2026-02-03 06:08:54 -0600

    Modifying ingest_data.py module to create input/output directories using in-built Python os module

--- modules/ingest_data.py
+++ modules/ingest_data.py
@@ -1,7 +1,29 @@
+# DO NOT DELETE
+# def ingest_data():
+#         try:
+#             import pandas as pd
+#             print("Pandas imported successfully!")
+#         except:
+#              print("EXCEPTION - Pandas import not successfull.")
+#         else:
+#             # df = pd.read_csv("test_directory/dirty_cafe_sales-csv.csv")
+#             df = pd.read_csv(r"data/raw_data/messy_crm_dataset_before_cleansing.csv")
+#             df_2 = df.copy()
+#             df_2.info()
+#             print("ingest data function ran successfully! RETURNING file to function!")
+#             return df_2
+
 def ingest_data():
         try:
             import pandas as pd
             print("Pandas imported successfully!")
+
+            # DEFINE INPUT / OUPUT DIRECTORIES
+            input_folder = "raw_data"
+            output_folder = "cleaned_data"
+
+            # Create output folder if it doesn't exist
+            os.makedirs(output_folder, exist_ok=True)
         except:
              print("EXCEPTION - Pandas import not successfull.")
         else:

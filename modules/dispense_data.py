f7d9b3e10fa5d05c63b61bcd140c4da9866ac2fc
colinrodney <bigboi0000@gmail.com>
2026-02-04 17:35:09 -0600

    Pushing addition of dispense_data.py module to development branch. This module will be responsible for handling output of cleansed data files to their designated location.

--- modules/dispense_data.py
+++ modules/dispense_data.py
@@ -1,17 +1,17 @@
-def dispense_data(df_2):
+def dispense_data(df_2, filename):
         try:
-            import pandas as pd
-            print("Pandas imported successfully!")
+            # import pandas as pd
+            # print("Pandas imported successfully!")
 
             import os
             print("os module imported successfully")
 
         except:
-             print("EXCEPTION - Pandas import not successfull.")
+            #  print("EXCEPTION - Pandas import not successfull.")
              print("EXCEPTION -os module import not successfull.")
         else:
             # DEFINE INPUT / OUPUT DIRECTORIES
-            input_folder = r"data/raw_data" 
+            # input_folder = r"data/raw_data" 
             output_folder = r"data/cleaned_data"
 
             # Create output folder if it doesn't exist
@@ -19,18 +19,18 @@ def dispense_data(df_2):
 
             # PROCESS EACH CSV IN FOLDER
 
-        for filename in os.listdir(input_folder):
-            print("FILENAME:'\n'",filename)
-            if filename.endswith(".csv"):
-                file_path = os.path.join(input_folder, filename)
-                print("FILEPATH: " '\n', file_path)
+        # for filename in os.listdir(input_folder):
+        #     print("FILENAME:'\n'",filename)
+        #     if filename.endswith(".csv"):
+        #         file_path = os.path.join(input_folder, filename)
+        #         print("FILEPATH: " '\n', file_path)
 
-            # df = pd.read_csv("test_directory/dirty_cafe_sales-csv.csv")
-            # df = pd.read_csv(r"data/raw_data/messy_crm_dataset_before_cleansing.csv")
-            df = pd.read_csv(file_path)
-            df_2 = df.copy()
-            df_2.info()
-            print("ingest data function ran successfully! RETURNING file to function!")
+        #     # df = pd.read_csv("test_directory/dirty_cafe_sales-csv.csv")
+        #     # df = pd.read_csv(r"data/raw_data/messy_crm_dataset_before_cleansing.csv")
+        #     df = pd.read_csv(file_path)
+        #     df_2 = df.copy()
+        #     df_2.info()
+        #     print("ingest data function ran successfully! RETURNING file to function!")
 
             output_path = os.path.join(output_folder, f"cleaned_{filename}")
             print("OUTPUT_PATH: " '\n', output_path)

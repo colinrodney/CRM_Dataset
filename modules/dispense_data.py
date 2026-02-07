7b751f91691425eedb15a0f29180d377cee6fc4c
colinrodney <bigboi0000@gmail.com>
2026-02-07 09:07:35 -0600

    Removed unnecessary comments from code + modified output_folder filepath back to relative filepath data/cleaned_data (.csv file will upload from this file location such that a downloadable artifact can be produced via github actions.

--- modules/dispense_data.py
+++ modules/dispense_data.py
@@ -1,42 +1,24 @@
 def dispense_data(df_2, filename):
-        try:
-            # import pandas as pd
-            # print("Pandas imported successfully!")
+    import os
+        
+    # output_folder = r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_automations\CRM_Dataset\data\cleaned_data\cleaned_messy_crm_dataset_after_cleansing.csv"
+    output_folder = r"data/cleaned_data"
 
-            import os
-            print("os module imported successfully")
+    try:
+        # Create output folder if it doesn't exist already
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder, exist_ok=True)
+            print(f"Created new directory at: {output_folder}")
 
-        except:
-            #  print("EXCEPTION - Pandas import not successfull.")
-             print("EXCEPTION -os module import not successfull.")
-        else:
-            # DEFINE INPUT / OUPUT DIRECTORIES
-            # input_folder = r"data/raw_data" 
-            # # output_folder = r"data/cleaned_data"
-            # output_folder = r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_automations\CRM_Dataset\data\cleaned_data"
-            output_folder = r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_automations\CRM_Dataset\data\cleaned_data\cleaned_messy_crm_dataset_after_cleansing.csv"
+        # Construct full file path
+        output_path = os.path.join(output_folder, f"cleaned_{filename}")
+        print("OUTPUT_PATH: " '\n', output_path)
 
-            # Create output folder if it doesn't exist
-            if not os.path.exists(output_folder):
-                os.makedirs(output_folder, exist_ok=True)
+        # Write DataFrame to a CSV file in the output folder
+        df_2.to_csv(output_folder, index=False)
+        print(f"SUCCESS! File is now at: {output_path}")
 
-            # PROCESS EACH CSV IN FOLDER
-
-        # for filename in os.listdir(input_folder):
-        #     print("FILENAME:'\n'",filename)
-        #     if filename.endswith(".csv"):
-        #         file_path = os.path.join(input_folder, filename)
-        #         print("FILEPATH: " '\n', file_path)
-
-        #     # df = pd.read_csv("test_directory/dirty_cafe_sales-csv.csv")
-        #     # df = pd.read_csv(r"data/raw_data/messy_crm_dataset_before_cleansing.csv")
-        #     df = pd.read_csv(file_path)
-        #     df_2 = df.copy()
-        #     df_2.info()
-        #     print("ingest data function ran successfully! RETURNING file to function!")
-
-                output_path = os.path.join(output_folder, f"cleaned_{filename}")
-                print("OUTPUT_PATH: " '\n', output_path)
-                df_2.to_csv(output_folder, index=False)
-
-                return df_2
+        return df_2
+    
+    except Exception as e:
+        print(f"FAILED to write file. Error: {e}")

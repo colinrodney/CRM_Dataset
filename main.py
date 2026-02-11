a3d55762fb38b91a9ca2ffe63f42434d5b5f3dfd
colinrodney <bigboi0000@gmail.com>
2026-02-11 06:19:23 -0600

    Add import manage_phone_number module.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import modules.duplicates
 import modules.email_validate
 import modules.fill_missing_values
 import modules.dispense_data
+import modules.manage_phone_number
 
 # Invoke functions
 # modules.module1.func1()
@@ -18,6 +19,7 @@ df_2 = modules.duplicates.show_duplicates(df_2)
 # df_2 = modules.duplicates.drop_duplicates()
 df_2 = modules.email_validate.email_formatted_properly(df_2)
 df_2 = modules.fill_missing_values.fill_missing_values(df_2)
+df_2 = modules.manage_phone_number.standardize_phone_numbers(df_2)
 
 # dispense_data() accepts dataframe and filename as arguments from ingest_data()
 modules.dispense_data.dispense_data(df_2, filename)

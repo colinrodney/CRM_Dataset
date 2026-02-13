26e765a7cca24d693eb60746dca17aa449010b85
colinrodney <bigboi0000@gmail.com>
2026-02-13 06:34:14 -0600

    Modified code to resolve regex template error where variable named replacement was properly formatted as a regex digit selector but when used as argument to re.sub method the digit selector syntax is an invalid escape sequence for replacing: SOLUTION - Add Back-references to replacement string.

--- modules/manage_phone_number.py
+++ modules/manage_phone_number.py
@@ -1,40 +1,58 @@
-# STANDARDIZE PHONE NUMBERS
+# # STANDARDIZE PHONE NUMBERS
+# def standardize_phone_numbers(df_2):
+
+# DO NOT DELETE!
+#     # IMPORT REGEX MODULE
+#     import re
+#     phone_number_pattern = re.compile(r"[\+\s\(\)-.]")
+
+#     #Show data type of phone number column
+#     df_2.info()
+
+#     # REMOVE ALL NON-DIGIT CHARS FROM PHONE NUMBERS
+#     # Overwrite the original phone number column with the cleaned version
+#     # df_2["columnName"] = df_2["columnName"].apply(lambda x: re.sub(r'[(),\s\.\-]', '', x))
+#     df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(r'[(),\s\.\-]', '',x))
+
+
+#     # # REMOVE +1 FROM BEGINNING OF PHONE NUMBERS
+#     # plus_one = r"^\+\d"
+#     # plus_one_formatted = ""
+#     # # df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(plus_one, plus_one_formatted, x))
+#     # df_2["office_phone"] = df_2["office_phone"].apply(lambda x: re.sub(plus_one, plus_one_formatted, x))
+#     # df_2["mobile_phone"] = df_2["mobile_phone"].apply(lambda x: re.sub(plus_one, plus_one_formatted, x))
+
+#     # Add +1 TO BEGINNING OF PHONE NUMBERS
+#     number_pattern = r"(\d{3})(\d{3})(\d{4})"
+#     plus_one_prefix = "+1"
+#     formatted_number = plus_one_prefix + number_pattern
+#     # df_2["columnName"] = df_2["columnName"].apply(lambda x: re.sub(number_pattern, formatted_number, x))
+#     df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(number_pattern, formatted_number, x))
+
+
+#     #Format numbers E164 Format
+#     # # INSERT HYPHENS INTO PHONE NUMBERS USING BACK REFERENCES IN FORMATTED / REPLACEMENT PATTERN
+#     # search_pattern = r"(\d{3})(\d{3})(\d{4})"
+#     # formatted_pattern = r"\1-\2-\3"
+#     # # df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(search_pattern, formatted_pattern, x))
+#     # df_2["office_phone"] = df_2["office_phone"].apply(lambda x: re.sub(search_pattern, formatted_pattern, x))
+#     # df_2["mobile_phone"] = df_2["mobile_phone"].apply(lambda x: re.sub(search_pattern, formatted_pattern, x))
+
+#     return df_2
+
 def standardize_phone_numbers(df_2):
-
-    # IMPORT REGEX MODULE
     import re
-    phone_number_pattern = re.compile(r"[\+\s\(\)\-\.]")
+    
+    # Ensure the column exists and convert to string to avoid 'NoneType' errors
+    if "phone_number" in df_2.columns:
+        # 1. Strip all non-digit characters
+        df_2["phone_number"] = df_2["phone_number"].astype(str).str.replace(r'[(),\s\.\-]', '', regex=True)
 
-    #Show data type of phone number column
-    df_2.info()
-
-    # REMOVE ALL NON-DIGIT CHARS FROM PHONE NUMBERS
-    # Overwrite the original phone number column with the cleaned version
-    # df_2["columnName"] = df_2["columnName"].apply(lambda x: re.sub(r'[(),\s\.\-]', '', x))
-    df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(r'[(),\s\.\-]', '',x))
-
-
-    # # REMOVE +1 FROM BEGINNING OF PHONE NUMBERS
-    # plus_one = r"^\+\d"
-    # plus_one_formatted = ""
-    # # df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(plus_one, plus_one_formatted, x))
-    # df_2["office_phone"] = df_2["office_phone"].apply(lambda x: re.sub(plus_one, plus_one_formatted, x))
-    # df_2["mobile_phone"] = df_2["mobile_phone"].apply(lambda x: re.sub(plus_one, plus_one_formatted, x))
-
-    # Add +1 TO BEGINNING OF PHONE NUMBERS
-    number_pattern = r"(\d{3})(\d{3})(\d{4})"
-    plus_one_prefix = "+1"
-    formatted_number = plus_one_prefix + number_pattern
-    # df_2["columnName"] = df_2["columnName"].apply(lambda x: re.sub(number_pattern, formatted_number, x))
-    df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(number_pattern, formatted_number, x))
-
-
-    #Format numbers E164 Format
-    # # INSERT HYPHENS INTO PHONE NUMBERS USING BACK REFERENCES IN FORMATTED / REPLACEMENT PATTERN
-    # search_pattern = r"(\d{3})(\d{3})(\d{4})"
-    # formatted_pattern = r"\1-\2-\3"
-    # # df_2["phone_number"] = df_2["phone_number"].apply(lambda x: re.sub(search_pattern, formatted_pattern, x))
-    # df_2["office_phone"] = df_2["office_phone"].apply(lambda x: re.sub(search_pattern, formatted_pattern, x))
-    # df_2["mobile_phone"] = df_2["mobile_phone"].apply(lambda x: re.sub(search_pattern, formatted_pattern, x))
+        # 2. Add +1 using back-references (\1\2\3)
+        # We look for 10 digits and wrap them in groups
+        search_pattern = r"(\d{3})(\d{3})(\d{4})"
+        replacement = r"+1\1\2\3"
+        
+        df_2["phone_number"] = df_2["phone_number"].str.replace(search_pattern, replacement, regex=True)
 
     return df_2

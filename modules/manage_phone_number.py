94ee34f8ce3ecdfe8dea989f1cfe7b2bb49a3981
colinrodney <bigboi0000@gmail.com>
2026-02-16 09:59:02 -0600

    Removed decision structure on line 60

--- modules/manage_phone_number.py
+++ modules/manage_phone_number.py
@@ -57,14 +57,14 @@ def standardize_phone_numbers(df_2):
 
         # 3. Add +1 using back-references (\1\2\3) for numbers that do not already start with 1 (to avoid adding +1 to numbers that already have it)
         # We look for 10 digits and wrap them in groups
-        if ~df_2["phone_number"].str.startswith("1").any():
-            search_pattern = r"(\d{3})(\d{3})(\d{4})"
-            replacement = r"+1\1\2\3"
-        # else:
-        #     # 2. Clean: Collapse leading 1s (Captures numbers such as 11056744898 and converts them to 1056744898)
-        #     df_2['phone_number'] = df_2['phone_number'].astype(str).str.replace(r'^1+', '1', regex=True)
+        search_pattern = r"(\d{3})(\d{3})(\d{4})"
+        replacement = r"+1\1\2\3"
+        # if ~df_2["phone_number"].str.startswith("1").any():
+        # # else:
+        # #     # 2. Clean: Collapse leading 1s (Captures numbers such as 11056744898 and converts them to 1056744898)
+        # #     df_2['phone_number'] = df_2['phone_number'].astype(str).str.replace(r'^1+', '1', regex=True)
             
 
-            df_2["phone_number"] = df_2["phone_number"].str.replace(search_pattern, replacement, regex=True)
+        df_2["phone_number"] = df_2["phone_number"].str.replace(search_pattern, replacement, regex=True)
 
     return df_2
